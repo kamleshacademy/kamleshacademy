9ad9271914477ce96c98ad1dd078ada4dc92ad0c
kamleshacademy <92029905+kamleshacademy@users.noreply.github.com>
2021-10-06 17:34:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+   <select name="Gender" id="">
+       <option value="Select"></option>
+       <option value="Male">Male</option>
+       <option value="Female">Female</option>
+       <option value="Other">Other</option>
+   </select>
+   <bdo dir="rtl">Ram Kabir</bdo>
+   <abbr title="Central Pocessing Unit">CPU</abbr>
+   <em>This</em> is
+   2<sup>2</sup>
+   <var>H</var><sub>2</sub><var>O</var>
+
+
+   <br>
+   <dfn title="Lorem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam sed perspiciatis excepturi odit iste fugit.</dfn>
+</body>
+</html>
